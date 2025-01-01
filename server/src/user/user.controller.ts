72ad5c05cc871d1594a8b579701875da381536b7
Roko <rokoponjarac2@gmail.com>
2025-01-01 11:42:18 +0100

    make user route with data but not add to database

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto';
+
+@Controller('user')
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    @Post()
+    makeUser(@Body() dto: CreateUserDto) {
+        return this.userService.makeUser()
+    }
+}
