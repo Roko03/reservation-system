b8305706e0320dda23e87d7ff1b21899d99b8434
Roko <rokoponjarac2@gmail.com>
2025-01-02 16:44:53 +0100

    make public guard and add access token guard to everything

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './common/guards';
 
 @Module({
   imports: [UserModule, PrismaModule, AuthModule],
-  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -5,25 +5,26 @@ import { Tokens } from './types';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from "express"
 import { AtGuard, RtGuard } from '../common/guards';
-import { GetCurrentUser, GetCurrentUserId } from '../common/decorators';
+import { GetCurrentUser, GetCurrentUserId, Public } from '../common/decorators';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @Public()
     @Post('signup')
     @HttpCode(HttpStatus.CREATED)
     signUp(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signUp(dto)
     }
 
+    @Public()
     @Post('signin')
     @HttpCode(HttpStatus.OK)
     signIn(@Body() dto: SignInDto): Promise<Tokens> {
         return this.authService.signIn(dto)
     }
 
-    @UseGuards(AtGuard)
     @Post('logout')
     @HttpCode(HttpStatus.OK)
     logout(@GetCurrentUserId() userId: string) {
