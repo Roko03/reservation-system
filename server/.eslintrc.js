81de4eeb79072324ff0f330eb178b156265a37a1
Roko <rokoponjarac2@gmail.com>
2025-01-02 13:29:04 +0100

    make all function for auth

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,4 +1,7 @@
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
 module.exports = {
+  eslintPluginPrettierRecommended,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
@@ -21,11 +24,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
-      },
-    ],
+        "singleQuote": true,
+        "parser": "flow"
+      }
+    ]
   },
 };
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('/signup')
+    signUp() {
+        this.authService.signUp()
+    }
+
+    @Post('/singin')
+    singIn() {
+        this.authService.singIn()
+    }
+
+    @Post('/logout')
+    logout() {
+        this.authService.logout()
+    }
+
+    @Post('/refresh')
+    refreshTokens() {
+        this.authService.refreshTokens()
+    }
+}
