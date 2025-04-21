2aeaf7801c3aacf94a583cbbfb3c7786faf6dd27
Roko Ponjarac <72657685+Roko03@users.noreply.github.com>
2025-04-21 10:53:03 +0200

    #7: Set up routes (#12)

---
+++ client/src/views/Object/Object.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Object = () => (
+  <div>
+    <h1>Object</h1>
+  </div>
+);
+
+export default Object;
---
+++ client/src/views/Objects/Objects.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Objects = () => <div>Objects</div>;
+
+export default Objects;
