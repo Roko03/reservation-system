0aa1f32a3c5249f3f367018097490711540911f1
Roko <rokoponjarac2@gmail.com>
2025-04-21 10:52:04 +0200

    #7: Set up routes

--- client/src/routers/AppRouter.tsx
+++ client/src/routers/AppRouter.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Admin from '@/views/Admin';
+import Calendar from '@/views/Calendar';
 import Home from '@/views/Home';
+import MyReservations from '@/views/MyReservations';
+import Object from '@/views/Object';
+import Objects from '@/views/Objects';
+import Profile from '@/views/Profile';
+import Reservation from '@/views/Reservation';
+import Reservations from '@/views/Reservations';
+import User from '@/views/User';
+import Users from '@/views/Users';
 
 const AppRouter: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="my-reservations" element={<MyReservations />} />
+      <Route path="objects">
+        <Route index element={<Objects />} />
+        <Route path=":id" element={<Object />} />
+      </Route>
+      <Route path="reservations">
+        <Route index element={<Reservations />} />
+        <Route path=":id" element={<Reservation />} />
+      </Route>
+      <Route path="admin">
+        <Route index element={<Admin />} />
+        <Route path="objects">
+          <Route index element={<Objects />} />
+          <Route path=":id" element={<Object />} />
+        </Route>
+        <Route path="reservations">
+          <Route index element={<Reservations />} />
+          <Route path=":id" element={<Reservation />} />
+        </Route>
+        <Route path="users">
+          <Route index element={<Users />} />
+          <Route path=":id" element={<User />} />
+        </Route>
+        <Route path="calendar" element={<Calendar />} />
+      </Route>
       <Route path="*" element={<div>Not found</div>} />
     </Routes>
   </BrowserRouter>
---
+++ client/src/views/Calendar/Calendar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Calendar = () => (
+  <div>
+    <h1>Calendar</h1>
+    <p>Calendar page content goes here.</p>
+  </div>
+);
+
+export default Calendar;
