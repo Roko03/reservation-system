c482e4170a2844279d22c2fde42f8fe27857081e
Roko Ponjarac <72657685+Roko03@users.noreply.github.com>
2025-06-05 00:22:01 +0200

    #28: Make calendar page (#29)

--- client/src/views/Calendar/Calendar.tsx
+++ client/src/views/Calendar/Calendar.tsx
@@ -1,12 +1,89 @@
-import React from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { Calendar as BigCalendar, Views, dayjsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { Container } from '@mui/material';
+import dayjs, { Dayjs } from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
 
 import Layout from '@/components/Layout';
+import { PAGE_NUMBER } from '@/config/constants.config';
+import { ReservationStatus } from '@/model/reservation.model';
+import useQueryParams from '@/utils/hooks/useQueryParams';
+import DateTime from '@/utils/static/DateTime';
+import { getReservations } from '@/valtio/reservations/reservations.action';
+import { useReservationStore } from '@/valtio/reservations/reservations.store';
 
-const Calendar = () => (
-  <Layout>
-    <h1>Calendar</h1>
-    <p>Calendar page content goes here.</p>
-  </Layout>
+import './big-calendar.scss';
+
+dayjs.extend(timezone);
+
+const djLocalizer = dayjsLocalizer(dayjs);
+
+const EventComponent = ({ event }: { event: { title: string } }) => (
+  <div style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{event.title}</div>
 );
+
+const Calendar = () => {
+  const { pageNumber } = useQueryParams();
+  const { reservations } = useReservationStore();
+
+  const [currentDate, setCurrentDate] = useState<Dayjs>(DateTime.now());
+
+  const handleNavigate = (date: Date) => {
+    setCurrentDate(dayjs(date));
+  };
+
+  const events = useMemo(
+    () =>
+      reservations.map(r => {
+        const timeString = dayjs(r.time).format('HH:mm');
+        const start = dayjs(`${r.date.split('T')[0]}T${timeString}`)
+          .add(-3600000)
+          .toDate();
+        const end = dayjs(start).add(1, 'hour').toDate();
+
+        return {
+          title: `${r.object.name}`,
+          start,
+          end,
+          allDay: false,
+          resource: r,
+        };
+      }),
+    [reservations]
+  );
+
+  useEffect(() => {
+    const page = pageNumber - PAGE_NUMBER;
+
+    const startDate = DateTime.startOfWeek(currentDate);
+    const endDate = DateTime.endOfWeek(currentDate);
+
+    getReservations(page, { dateFrom: startDate, dateTo: endDate, status: ReservationStatus.APPROVED });
+  }, [currentDate, pageNumber]);
+
+  return (
+    <Layout>
+      <Container maxWidth={false}>
+        <BigCalendar
+          localizer={djLocalizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          views={[Views.WEEK]}
+          defaultView={Views.WEEK}
+          aria-label="Reservation Calendar"
+          popup
+          onNavigate={handleNavigate}
+          min={dayjs().hour(8).minute(0).toDate()}
+          components={{
+            event: EventComponent,
+          }}
+        />
+      </Container>
+    </Layout>
+  );
+};
 
 export default Calendar;
